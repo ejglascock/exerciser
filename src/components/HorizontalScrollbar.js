36686cab507fd0ce432631c85b1de0584157075c
ejglascock <ejglascock@gmail.com>
2022-11-14 14:20:48 -0800

    HorizontalScrollbar maps out each bodypart fetched earlier. Created BodyPart component file.

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,8 +1,21 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
-const HorizontalScrollbar = () => {
+const HorizontalScrollbar = ({ data }) => {
   return (
-    <div>HorizontalScrollbar</div>
+    <div>
+        {data.map((item) => (
+            <Box 
+                key={item.id || item}
+                itemID={item.id || item}
+                title={item.id || item}
+                m="0 40px"
+            >
+                {item}
+            </Box>
+            )
+        )}
+    </div>
   )
 }
 

5efe0e870e8830fbe60fd61f6cbe2a25e8c461ec
ejglascock <ejglascock@gmail.com>
2022-11-14 15:16:37 -0800

    BodyPart component now shows an image but needs to be aligned horizontally

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,25 @@
+import { Stack } from '@mui/material'
 import React from 'react'
 
-const BodyPart = () => {
+const BodyPart = ({ item, bodyPart, setBodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+        type="button"
+        alignItems="center"
+        justifyContent="center"
+        className="bodyPart-card"
+        sx={{
+            borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+            background: '#fff',
+            borderBottomLeftRadius: '20px',
+            width: '270px',
+            height: '280px',
+            cursor: 'pointer',
+            gap: '47px'
+        }}
+    >
+        <img alt="dumbbell" styles={{ width: '40px', height: '40px'}} />
+    </Stack>
   )
 }
 

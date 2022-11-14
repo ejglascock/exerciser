7d6bf99a0260121f7d255156b5765f1b901351db
ejglascock <ejglascock@gmail.com>
2022-11-14 11:23:01 -0800

    setup useEffect to run once and fetch body part data. Created HorizontalScrollbar component file

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,22 @@
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
 const SearchExercises = () => {
   const [search, setSearch] = useState(''); 
   const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+        const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+        setBodyParts(['all', ...bodyPartsData]);
+    }
+
+    fetchExercisesData();
+  }, [])
 
   const handleSearch = async () => {
     if(search) {
@@ -73,6 +85,15 @@ const SearchExercises = () => {
             >
                 Search
             </Button>
+        </Box>
+        <Box
+            sx={{
+                position: 'relative',
+                width: '100%',
+                p: '20px'
+            }}
+        >
+            <HorizontalScrollbar data={bodyParts} />
         </Box>
     </Stack>
   )
